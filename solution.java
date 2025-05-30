092fbbf2e8b1b1ee7e428ad5f0ccd75db01581d7
Vishnu6209 <vishnuvardhanreddy.mukka@gmail.com>
2025-05-30 16:10:19 +0530

    Add files via upload

---
+++ solution.java
@@ -0,0 +1,21 @@
+package projectofjava;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class solution {
+	public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        try{
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int c=a/b;
+            System.out.println(c);
+        } catch (InputMismatchException e) {
+            System.out.println("java.util.InputMismatchException");
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
