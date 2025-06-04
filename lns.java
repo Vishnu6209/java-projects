f2bc0e2c37b30f21ee73691be6e770fadcdc6973
Vishnu6209 <vishnuvardhanreddy.mukka@gmail.com>
2025-06-04 14:25:20 +0530

    Add files via upload

---
+++ lns.java
@@ -0,0 +1,11 @@
+public class lns{
+public static void main(String[] args){
+int[] numbers={5,8,2,3,45,22,1,10}
+int target =10;
+boolean found=false;
+for(int i=0;i<numbers.length;i++){
+System.out.println("found:"+" "+target+" at index"+i);
+found=true;
+break; }
+}
+}
