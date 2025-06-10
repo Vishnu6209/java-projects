7a7e2592f003952b4d51a85d3b3e528fd27b18f8
Vishnu6209 <vishnuvardhanreddy.mukka@gmail.com>
2025-06-10 12:21:22 +0530

    Add files via upload

---
+++ QuickSort.java
@@ -0,0 +1,45 @@
+package projectofjava;
+
+public class QuickSort {
+	static void quicksort(int arr[], int low, int high) {
+		if(low<high) {
+			int pivotIndex = partition(arr,low,high);
+			quicksort(arr, low, pivotIndex-1);
+			quicksort(arr, pivotIndex+1, high);
+		}
+	}
+	static int partition(int[] arr,int low,int high) {
+		int pivot=arr[high];
+		int i=low-1;
+		for(int j=low;j<high;j++) {
+			if(arr[j]<pivot) {
+				i++;
+				int temp=arr[i];
+				arr[i]=arr[j];
+				arr[j]=temp;
+			}
+		}
+		int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1; 
+	}
+	static void printArray(int[] arr) {
+		for(int value:arr) {
+			System.out.println(value+" ");
+		}
+		System.out.println();
+	}
+	public static void main(String[] args) {
+		int[] numbers= {5,2,9,1,6};
+		int low=0;
+		int high=numbers.length-1;
+		System.out.print("before sorting ");
+		QuickSort obj1=new QuickSort();
+		obj1.printArray(numbers);
+		obj1.quicksort(numbers, low, high);
+		System.out.print("after sorted ");
+		obj1.printArray(numbers);
+	}
+}
